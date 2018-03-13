9cd032e3a5bebf32bd05057b715334abd3ccd55d
Yawen Zhang <Yawen.Zhang@colorado.edu>
2018-03-12 22:26:56 -0600

    vis 1 done

--- Submission/preprocessing.py
+++ Submission/preprocessing.py
@@ -6,28 +6,29 @@ import math
 NCWIT_Data = pd.read_csv("data/NCWIT_DataV2_RawData.csv")
 # print(NCWIT_Data)
 
-# compute per School Year race distribution (Female)
-# 7 data attributes used
-NCWIT_Data_Female_Race_Year = NCWIT_Data.groupby("School Year")["Totals, Female: Asian (Tot. F)", "Totals, Female: Black/African American (Tot. F)", "Totals, Female: Hispanics of any race (Tot. F)", "Totals, Female: American Indian/Alaska Native (Tot. F)", "Totals, Female: Native Hawaiian/Other Pacific Islander (Tot. F)", "Totals, Female: White (Tot. F)"].sum()
+# compute Total Enrollment
+NCWIT_Data['Totals, Female: Enrollment'] = NCWIT_Data['Totals, Female: Enrolled in DIFFERENT MAJOR (Tot. F)'] + NCWIT_Data['Totals, Female: Enrolled in SAME MAJOR (Tot. F)']
+NCWIT_Data['Totals, Male: Enrollment'] = NCWIT_Data['Totals, Male: Enrolled in DIFFERENT MAJOR (Tot. M)'] + NCWIT_Data['Totals, Male: Enrolled in SAME MAJOR (Tot. M)']
 
-NCWIT_Data_Female_Race_Year.to_csv("data/NCWIT_Data_Female_Race_Year.csv")
+# VISUALIZATION 1
+# attributes
+vis_1_Female_attr = ["Totals, Female: Graduated (Tot. F)", "Totals, Female: Left Institution (not graduated) (Tot. F)", "Totals, Female: Enrollment"]
+# export
+vis_1_Female = NCWIT_Data.groupby("School Year")[vis_1_Female_attr].sum()
+# compute rate (%)
+vis_1_Female['Female Graduated Rate (%)'] = vis_1_Female.apply(lambda row: row[0]/row[2]*100, axis = 1)
+vis_1_Female['Female Dropout Rate (%)'] = vis_1_Female.apply(lambda row: row[1]/row[2]*100, axis = 1)
+# print(vis_1_Female)
 
-NCWIT_Data_Male_Race_Year = NCWIT_Data.groupby("School Year")["Totals, Male: Asian (Tot. M)", "Totals, Male: Black/African American (Tot. M)", "Totals, Male: Hispanics of any race (Tot. M)", "Totals, Male: American Indian/Alaska Native (Tot. M)", "Totals, Male: Native Hawaiian/Other Pacific Islander (Tot. M)", "Totals, Male: White (Tot. M)"].sum()
+# attributes
+vis_1_Male_attr = ["Totals, Male: Graduated (Tot. M)", "Totals, Male: Left Institution (not graduated) (Tot. M)", "Totals, Male: Enrollment"]
+# export
+vis_1_Male = NCWIT_Data.groupby("School Year")[vis_1_Male_attr].sum()
+# compute rate (%)
+vis_1_Male['Male Graduated Rate (%)'] = vis_1_Male.apply(lambda row: row[0]/row[2]*100, axis = 1)
+vis_1_Male['Male Dropout Rate (%)'] = vis_1_Male.apply(lambda row: row[1]/row[2]*100, axis = 1)
 
-NCWIT_Data_Male_Race_Year.to_csv("data/NCWIT_Data_Male_Race_Year.csv")
-
-#compute Female VS. Male per school year in each majors
-#When comes with missing data, delete the row with all NaN and fill in the blank with "0"
-NCWIT_Data_Female_Majors_Year = NCWIT_Data.groupby(["School Year","Major Program Name"])["Totals, Female: Asian (Tot. F)", "Totals, Female: Black/African American (Tot. F)", "Totals, Female: Hispanics of any race (Tot. F)", "Totals, Female: American Indian/Alaska Native (Tot. F)", "Totals, Female: Native Hawaiian/Other Pacific Islander (Tot. F)", "Totals, Female: White (Tot. F)"].sum().dropna(axis=0, how='all').fillna(0)
-NCWIT_Data_Male_Majors_Year = NCWIT_Data.groupby(["School Year","Major Program Name"])["Totals, Male: Asian (Tot. M)", "Totals, Male: Black/African American (Tot. M)", "Totals, Male: Hispanics of any race (Tot. M)", "Totals, Male: American Indian/Alaska Native (Tot. M)", "Totals, Male: Native Hawaiian/Other Pacific Islander (Tot. M)", "Totals, Male: White (Tot. M)"].sum().dropna(axis=0, how='all').fillna(0)
-
-NCWIT_Data_Female_Majors_Year.to_csv("data/NCWIT_Data_Female_Majors_Year.csv")
-NCWIT_Data_Male_Majors_Year.to_csv("data/NCWIT_Data_Male_Majors_Year.csv")
-
-#compute Female VS. Male per school year in each majors in each degrees
-#When comes with missing data, delete the row with all NaN and fill in the blank with "0"
-#NCWIT_Data_Female_degree_Year = NCWIT_Data.groupby(["School Year","Major Program Name"])["Freshmen, Female: Asian (Fshm F)", "Freshmen, Female: Black/African American (Fshm F)", "Freshmen, Female: Hispanics of any race (Fshm F)", "Freshmen, Female: American Indian/Alaska Native (Fshm F)", "Freshmen, Female: Native Hawaiian/Other Pacific Islander (Fshm F)", "Freshmen, Female: White (Fshm F)","Sophomores, Female: Asian (Sph. F)", "Sophomores, Female: Black/African American (Sph. F)", "Sophomores, Female: Hispanics of any race (Sph. F)", "Sophomores, Female: American Indian/Alaska Native (Sph. F)", "Sophomores, Female: Native Hawaiian/Other Pacific Islander (Sph. F)", "Sophomores, Female: White (Sph. F)","Juniors, Female: Asian (Jun. F)", "Juniors, Female: Black/African American (Jun. F)", "Juniors, Female: Hispanics of any race (Jun. F)", "Juniors, Female: American Indian/Alaska Native (Jun. F)", "Juniors, Female: Native Hawaiian/Other Pacific Islander (Jun. F)", "Juniors, Female: White (Jun. F)","Seniors, Female: Asian (Sen. F)", "Seniors, Female: Black/African American (Sen. F)", "Seniors, Female: Hispanics of any race (Sen. F)", "Seniors, Female: American Indian/Alaska Native (Sen. F)", "Seniors, Female: Native Hawaiian/Other Pacific Islander (Sen. F)", "Seniors, Female: White (Sen. F)","5th Yr Seniors, Female: Asian (5 Sen F)", "5th Yr Seniors, Female: Black/African American (5 Sen F)", "5th Yr Seniors, Female: Hispanics of any race (5 Sen F)", "5th Yr Seniors, Female: American Indian/Alaska Native (5 Sen F)", "5th Yr Seniors, Female: Native Hawaiian/Other Pacific Islander (5 Sen F)", "5th Yr Seniors, Female: White (5 Sen F)"].sum().dropna(axis=0, how='all').fillna(0)
-#NCWIT_Data_Male_degree_Year = NCWIT_Data.groupby(["School Year","Major Program Name"])["Freshmen, Male: Asian (Fshm M)", "Freshmen, Male: Black/African American (Fshm M)", "Freshmen, Male: Hispanics of any race (Fshm M)", "Freshmen, Male: American Indian/Alaska Native (Fshm M)", "Freshmen, Male: Native Hawaiian/Other Pacific Islander (Fshm M)", "Freshmen, Male: White (Fshm M)","Sophomores, Male: Asian (Sph. M)", "Sophomores, Male: Black/African American (Sph. M)", "Sophomores, Male: Hispanics of any race (Sph. M)", "Sophomores, Male: American Indian/Alaska Native (Sph. M)", "Sophomores, Male: Native Hawaiian/Other Pacific Islander (Sph. M)", "Sophomores, Male: White (Sph. M)","Juniors, Male: Asian (Jun. M)", "Juniors, Male: Black/African American (Jun. M)", "Juniors, Male: Hispanics of any race (Jun. M)", "Juniors, Male: American Indian/Alaska Native (Jun. M)", "Juniors, Male: Native Hawaiian/Other Pacific Islander (Jun. M)", "Juniors, Male: White (Jun. M)","Seniors, Male: Asian (Sen. M)", "Seniors, Male: Black/African American (Sen. M)", "Seniors, Male: Hispanics of any race (Sen. M)", "Seniors, Male: American Indian/Alaska Native (Sen. M)", "Seniors, Male: Native Hawaiian/Other Pacific Islander (Sen. M)", "Seniors, Male: White (Sen. M)","5th Yr Seniors, Male: Asian (5 Sen M)", "5th Yr Seniors, Male: Black/African American (5 Sen M)", "5th Yr Seniors, Male: Hispanics of any race (5 Sen M)", "5th Yr Seniors, Male: American Indian/Alaska Native (5 Sen M)", "5th Yr Seniors, Male: Native Hawaiian/Other Pacific Islander (5 Sen M)", "5th Yr Seniors, Male: White (5 Sen M)"].sum().dropna(axis=0, how='all').fillna(0)
-
-#NCWIT_Data_Female_degree_Year.to_csv("data/NCWIT_Data_Female_Majors_Race_Degree_Year.csv")
-#NCWIT_Data_Male_degree_Year.to_csv("data/NCWIT_Data_Male_Majors_Race_Degree_Year.csv")
+# combine Female & Male
+vis_1 = pd.concat([vis_1_Female, vis_1_Male], axis = 1)
+vis_1 = vis_1.dropna() # remove 2016-2017 NaN
+vis_1.to_csv("data/vis_1_Graduate_Dropout_rate_Year.csv")
